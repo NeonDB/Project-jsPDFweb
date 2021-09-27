914b1e44cc37dfc138ecb47e4884899cc149d47d
NeonDB <77829100+NeonDB@users.noreply.github.com>
2021-09-27 18:58:48 +0300

    Initial commit

---
+++ choosen.js
@@ -0,0 +1,15 @@
+function chooseparts1() {
+    let textdata = document.querySelector("div.container_block1")
+        textdata.classList.toggle("visible_element")
+        textdata.classList.toggle("hidenseek_element")
+}
+function chooseparts2() {
+    let textdata = document.querySelector("div.container_block2")
+        textdata.classList.toggle("visible_element")
+        textdata.classList.toggle("hidenseek_element")
+}
+function chooseparts3() {
+    let textdata = document.querySelector("div.container_block3")
+        textdata.classList.toggle("visible_element")
+        textdata.classList.toggle("hidenseek_element")
+}
