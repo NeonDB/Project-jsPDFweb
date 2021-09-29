8ee5f39733be0d66e78152399619eaa0234e224a
NeonDB <77829100+NeonDB@users.noreply.github.com>
2021-09-29 12:20:04 +0300

    chuchumfca

--- choosen.js
+++ choosen.js
@@ -13,3 +13,38 @@ function chooseparts3() {
         textdata.classList.toggle("visible_element")
         textdata.classList.toggle("hidenseek_element")
 }
+function chooseparts4() {
+    let textdata = document.querySelector("div.container_block4")
+        textdata.classList.toggle("visible_element")
+        textdata.classList.toggle("hidenseek_element")
+}
+function chooseparts5() {
+    let textdata = document.querySelector("div.container_block5")
+        textdata.classList.toggle("visible_element")
+        textdata.classList.toggle("hidenseek_element")
+}
+function chooseparts6() {
+    let textdata = document.querySelector("div.container_block6")
+        textdata.classList.toggle("visible_element")
+        textdata.classList.toggle("hidenseek_element")
+}
+function chooseparts7() {
+    let textdata = document.querySelector("div.container_block7")
+        textdata.classList.toggle("visible_element")
+        textdata.classList.toggle("hidenseek_element")
+}
+function chooseparts8() {
+    let textdata = document.querySelector("div.container_block8")
+        textdata.classList.toggle("visible_element")
+        textdata.classList.toggle("hidenseek_element")
+}
+function chooseparts9() {
+    let textdata = document.querySelector("div.container_block9")
+        textdata.classList.toggle("visible_element")
+        textdata.classList.toggle("hidenseek_element")
+}
+function chooseparts10() {
+    let textdata = document.querySelector("div.container_block10")
+        textdata.classList.toggle("visible_element")
+        textdata.classList.toggle("hidenseek_element")
+}
