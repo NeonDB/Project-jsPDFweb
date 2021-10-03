d0906a7153f5a882ad3e94fec25b622ba614e943
NeonDB <77829100+NeonDB@users.noreply.github.com>
2021-10-03 16:06:27 +0300

    03102021

--- choosen.js
+++ choosen.js
@@ -6,6 +6,9 @@ function asyncblock_toggle() {
     textdata.forEach((textdata) => {
         textdata.classList.toggle("hidenseek_element")
     })
+    let syncchbx = document.getElementById("check_part2")
+    if (syncchbx.checked==true) document.getElementById('check_part1').disabled = true
+    else document.getElementById('check_part1').disabled = false;
 }
 function syncblock_toggle() {
     let textdata = document.querySelectorAll("div.container_sync")
@@ -15,6 +18,9 @@ function syncblock_toggle() {
     textdata.forEach((textdata) => {
         textdata.classList.toggle("hidenseek_element")
     })
+    let syncchbx = document.getElementById("check_part1")
+    if (syncchbx.checked==true) document.getElementById('check_part2').disabled = true
+    else document.getElementById('check_part2').disabled = false;
 }
 function twice_camera_toggle() {
     let textdata = document.querySelector("div.container_camera")
@@ -28,23 +34,38 @@ function slot_entry_toggle() {
 }
 function bioidendifity_Examus_toggle() {
     let textdata = document.querySelector("div.container_blockExamus")
-        textdata.classList.toggle("visible_element")
-        textdata.classList.toggle("hidenseek_element")
+    textdata.classList.toggle("visible_element")
+    textdata.classList.toggle("hidenseek_element")
+    let syncchbx = document.getElementById("check_part5")
+    if (syncchbx.checked==true) document.getElementById('check_part6').disabled = true,
+    document.getElementById('check_part7').disabled = true
+    else document.getElementById('check_part6').disabled = false,
+    document.getElementById('check_part7').disabled = false;
 }
 function bioidendifity_EBS_toggle() {
     let textdata = document.querySelectorAll("div.container_blockEBS")
     textdata.forEach((textdata) => {
         textdata.classList.toggle("visible_element")
-
     })
     textdata.forEach((textdata) => {
         textdata.classList.toggle("hidenseek_element")
     })
+    let syncchbx = document.getElementById("check_part6")
+    if (syncchbx.checked==true) document.getElementById('check_part5').disabled = true,
+    document.getElementById('check_part7').disabled = true
+    else document.getElementById('check_part5').disabled = false,
+    document.getElementById('check_part7').disabled = false;
 }
 function bioidendifity_VLLuna_toggle() {
     let textdata = document.querySelector("div.container_blockVLLuna")
-        textdata.classList.toggle("visible_element")
-        textdata.classList.toggle("hidenseek_element")
+    textdata.classList.toggle("visible_element")
+    textdata.classList.toggle("hidenseek_element")
+    let syncchbx = document.getElementById("check_part7")
+    if (syncchbx.checked==true) document.getElementById('check_part5').disabled = true,
+    document.getElementById('check_part6').disabled = true
+    else document.getElementById('check_part5').disabled = false,
+    document.getElementById('check_part6').disabled = false;
+
 }
 function personal_data_consent_toggle() {
     let textdata = document.querySelector("div.container_personal_data_consent")
@@ -64,12 +85,44 @@ function integration_typeMoodle_toggle() {
     let textdata = document.querySelector("div.container_block_generation")
         textdata.classList.toggle("visible_element")
         textdata.classList.toggle("hidenseek_element")
+    let beginbutton = document.querySelector("div.container_moodleint_block")
+        beginbutton.classList.toggle("hidenseek_element")
+        beginbutton.classList.toggle("visible_element")
+
+    let syncchbx = document.getElementById("check_part9")
+        if (syncchbx.checked==true) document.getElementById('check_part10').disabled = true,
+        document.getElementById('check_part11').disabled = true
+        else document.getElementById('check_part10').disabled = false,
+        document.getElementById('check_part11').disabled = false;
 }
 function integration_typeAPI_toggle() {
     let textdata = document.querySelector("div.container_block_generation")
-        textdata.classList.add("hidenseek_element")
+        textdata.classList.toggle("visible_element")
+        textdata.classList.toggle("hidenseek_element")
+        let syncchbx = document.getElementById("check_part10")
+        if (syncchbx.checked==true) document.getElementById('check_part9').disabled = true,
+        document.getElementById('check_part11').disabled = true
+        else document.getElementById('check_part9').disabled = false,
+        document.getElementById('check_part11').disabled = false;
 }
 function integration_typeURLgeneration_toggle() {
     let textdata = document.querySelector("div.container_block_generation")
-        textdata.classList.add("hidenseek_element")
+        textdata.classList.toggle("hidenseek_element")
+        let syncchbx = document.getElementById("check_part11")
+        if (syncchbx.checked==true) document.getElementById('check_part9').disabled = true,
+        document.getElementById('check_part10').disabled = true,
+        document.getElementById('check_part4').disabled = true
+        else document.getElementById('check_part9').disabled = false,
+        document.getElementById('check_part10').disabled = false,
+        document.getElementById('check_part4').disabled = false;
+
+}
+function faq_block_toggle() {
+    let textdata = document.querySelectorAll("div.container_faq_block")
+    textdata.forEach((textdata) => {
+        textdata.classList.toggle("visible_element")
+    })
+    textdata.forEach((textdata) => {
+        textdata.classList.toggle("hidenseek_element")
+    })
 }
